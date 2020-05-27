edb57f80340b0f418915f28099ce20a5d2ec3532
Fany <aesanchez@intersphere.mx>
2020-05-27 13:42:17 -0500

    Corrección variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,9 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://saFany:6NcFYhZQQnub1Vd8@cluster0-c2uto.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
-urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
